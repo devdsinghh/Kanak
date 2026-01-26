b7acef2d4eeedfd6cdec691c19559e9790a33fe6
opk00637-glitch <opk00637@gmail.com>
2026-01-26 07:05:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 # ======================================================
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/yjee4247-code/shivi")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/opk00637-glitch/shivi")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
